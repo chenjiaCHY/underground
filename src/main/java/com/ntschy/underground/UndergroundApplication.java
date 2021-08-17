69d1c1ff5c301b95c4901559508ae313152e78ad
陈佳 <chenjia@chenjiadeMacBook-Pro.local>
2021-08-17 14:48:05 +0800

    项目初始化

---
+++ src/main/java/com/ntschy/underground/UndergroundApplication.java
@@ -0,0 +1,13 @@
+package com.ntschy.underground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UndergroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UndergroundApplication.class, args);
+	}
+
+}
