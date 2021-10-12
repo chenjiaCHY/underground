e90ed182bbb242a22da17678e25b44e1c0aae800
chenjiaCHY <chenjiachy@126.com>
2021-10-12 17:25:37 +0800

    修改增加项目接口

--- src/main/java/com/ntschy/underground/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/ntschy/underground/service/impl/ProjectServiceImpl.java
@@ -123,7 +123,9 @@ public class ProjectServiceImpl implements ProjectService {
         projectDao.addProject(projectRecord);
 
         // 将图纸文件名列表插入到FILE_UPLOAD表中
-        projectDao.addFiles(UploadFileType.PROJECT.getCode(), projectId, addProjectRequest.getFiles());
+        if (!CollectionUtils.isEmpty(addProjectRequest.getFiles())) {
+            projectDao.addFiles(UploadFileType.PROJECT.getCode(), projectId, addProjectRequest.getFiles());
+        }
 
         return new Result(true, "新增项目成功!!!");
     }
